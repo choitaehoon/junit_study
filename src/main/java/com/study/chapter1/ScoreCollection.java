c212b6ac23f702554574420ddcb1ba2469bf7e83
choitaehoon <xognsdl1230@naver.com>
2020-01-18 23:37:27 +0900

    큰 정수 일때 예외 처리

--- src/main/java/com/study/chapter1/ScoreCollection.java
+++ src/main/java/com/study/chapter1/ScoreCollection.java
@@ -2,19 +2,27 @@ package com.study.chapter1;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class ScoreCollection {
     private List<Scoreable> scores = new ArrayList<>();
+    private static int SIZE_ZERO = 0;
 
     public void add(Scoreable scoreable) {
+        Optional.ofNullable(scoreable).orElseThrow(IllegalArgumentException::new);
+
         scores.add(scoreable);
     }
 
     public int arithmeticMean() {
-        int total = scores.stream()
+        if (scores.size() == SIZE_ZERO) {
+            return SIZE_ZERO;
+        }
+
+        long total = scores.stream()
                 .mapToInt(Scoreable::getScore)
                 .sum();
 
-        return total / scores.size();
+        return (int)(total / scores.size());
     }
 }
--- src/test/java/com/study/chapter6/ScoreCollectionTest.java
+++ src/test/java/com/study/chapter6/ScoreCollectionTest.java
@@ -5,17 +5,13 @@ import static org.hamcrest.CoreMatchers.*;
 
 import com.study.chapter1.ScoreCollection;
 import org.junit.*;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.rules.ExpectedException;
+import org.junit.jupiter.api.DisplayName;
 
 public class ScoreCollectionTest {
 
     private ScoreCollection collection;
 
-    @Rule
-    public ExpectedException exception = ExpectedException.none();
-
-    @BeforeEach
+    @Before
     public void setup() {
         collection = new ScoreCollection();
     }
@@ -23,6 +19,28 @@ public class ScoreCollectionTest {
     @Test(expected=IllegalArgumentException.class)
     public void throwsExceptionWhenAddingNull() {
         collection.add(null);
+    }
+
+    @Test
+    public void answersZeroWhenNoElementsAdded() {
+        assertThat(collection.arithmeticMean(), equalTo(0));
+    }
+
+    @DisplayName("큰 정수를 다룰 때 Integer.MAX_VALUE 초과 할 경우")
+    @Test
+    public void dealsWithIntegerOverflow() {
+        collection.add(() -> Integer.MAX_VALUE);
+        collection.add(() -> 1);
+
+        assertThat(collection.arithmeticMean(), equalTo(1073741824));
+    }
+
+    @Test
+    public void doesNotProperlyHandleIntegerOverflow() {
+        collection.add(() -> Integer.MAX_VALUE);
+        collection.add(() -> 1);
+
+        assertTrue(collection.arithmeticMean() < 0);
     }
 
 }
