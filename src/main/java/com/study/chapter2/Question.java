61b30ce86ba32b269a107f3623203c69a24459a8
choitaehoon <xognsdl1230@naver.com>
2020-01-19 15:36:42 +0900

    Right-BICEP

--- src/main/java/com/study/chapter2/Question.java
+++ src/main/java/com/study/chapter2/Question.java
@@ -3,6 +3,7 @@ package com.study.chapter2;
 public abstract class Question {
     private String text;
     private String[] answerChoices;
+    private int id;
 
     public Question(String text, String[] answerChoices) {
         this.text = text;
@@ -19,6 +20,10 @@ public abstract class Question {
 
     public boolean match(Answer answer) {
         return false;
+    }
+
+    public int getId() {
+        return id;
     }
 
     abstract public boolean match(int expected, int actual);
--- src/test/java/com/study/chapter6/NewtonTest.java
+++ src/test/java/com/study/chapter6/NewtonTest.java
@@ -1,11 +1,22 @@
 package com.study.chapter6;
 
+import com.study.chapter2.Answer;
+import com.study.chapter2.Bool;
+import com.study.chapter2.BooleanQuestion;
+import com.study.chapter2.Profile;
+import com.study.chapter5.PercentileQuestion;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+
+import java.util.Collection;
+import java.util.function.Predicate;
 
 import static org.junit.Assert.*;
 import static org.hamcrest.number.IsCloseTo.*;
 
 public class NewtonTest {
+
+    private Profile profile;
 
     static class Newton {
         private static final double TOLERANCE = 1E-16;
@@ -18,11 +29,34 @@ public class NewtonTest {
             return approx;
         }
     }
+    
+    private long run(int times, Runnable func) {
+        long start = System.nanoTime();
+        for (int i = 0; i<times; ++i)
+            func.run();
+        long stop = System.nanoTime();
+        return (stop - start) / 1000000;
+    }
+
+    @Before
+    public void createProfile() {
+        profile = new Profile("Bull Hockey, Inc.");
+    }
+
+    int[] ids(Collection<Answer> answers) {
+        return answers.stream()
+            .mapToInt(a -> a.getQuestion().getId()).toArray();
+    }
 
     @Test
     public void squareRoot() {
         double result = Newton.squareRoot(250.0);
         assertThat(result * result, closeTo(250.0, Newton.TOLERANCE));
+    }
+
+    @Test
+    public void squareRootVerifiedUsingLibrary() {
+        assertThat(Newton.squareRoot(1969.0), closeTo(Math.sqrt(1969.0), Newton.TOLERANCE));
     }
 
 }
